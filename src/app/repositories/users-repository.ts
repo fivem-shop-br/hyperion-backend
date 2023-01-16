27360c4c1d91de53830a19db52affc5807b0d313
Fulano de Tal <F3ZINH0HG@gmail.com>
2023-01-16 16:56:36 -0300

    refactor(user): `findByEmail` new strategy.

--- src/app/repositories/users-repository.ts
+++ src/app/repositories/users-repository.ts
@@ -4,7 +4,7 @@ import type { User as UserPrisma } from '@prisma/client';
 export abstract class UserRepository {
   abstract findAll(): Promise<User[]>;
   abstract findById(userId: string): Promise<User>;
-  abstract findByEmail(userEmail: string): Promise<User>;
+  abstract findByEmail(userEmail: string): Promise<UserPrisma | null>;
   abstract delete(userId: string): Promise<User>;
   abstract create(user: User): Promise<UserPrisma>;
   abstract update(user: User): Promise<UserPrisma>;
