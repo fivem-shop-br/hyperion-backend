a9ae54bfd4c1dad48938f700ec2992fbcb50511e
Fulano de Tal <F3ZINH0HG@gmail.com>
2023-01-16 14:04:39 -0300

    chore(auth): create module, controller & service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { DatabaseModule } from '@infra/database/database.module';
 import { HttpModule } from '@infra/http/http.module';
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [HttpModule, DatabaseModule],
+  imports: [HttpModule, DatabaseModule, AuthModule],
 })
 export class AppModule {}
