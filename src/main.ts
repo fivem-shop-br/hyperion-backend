54ffe5ffd3857ab045e9dcebeab5b429bf9368a7
Fulano de Tal <F3ZINH0HG@gmail.com>
2023-01-19 00:10:37 -0300

    refactor: test cors.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
   await app.listen(3000);
 }
 bootstrap();
