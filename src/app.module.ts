c21dd95388d25efed09a58a504f06a95c9e78e6b
Fulano de Tal <F3ZINH0HG@gmail.com>
2023-01-18 22:56:28 -0300

    chore: error deploy in vercel #6.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { DatabaseModule } from '@infra/database/database.module';
+import { DatabaseModule } from './infra/database/database.module';
 import { HttpModule } from '@infra/http/http.module';
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,7 +2,7 @@ import { CreateUser } from '@app/use-cases/create-user';
 import { DeleteUserById } from '@app/use-cases/delete-user';
 import { FindAllUsers } from '@app/use-cases/find-users';
 import { UpdateUser } from '@app/use-cases/update-user';
-import { DatabaseModule } from '@infra/database/database.module';
+import { DatabaseModule } from '../database/database.module';
 import { Module } from '@nestjs/common';
 import { FindUserById } from 'src/app/use-cases/find-user';
 import { UsersController } from './controllers/users.controller';
