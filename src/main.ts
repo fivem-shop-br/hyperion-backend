548c14cf3ccb864fdef226e7e3bdc04e71d52a49
Fulano de Tal <F3ZINH0HG@gmail.com>
2023-01-18 23:53:35 -0300

    chore: cors in `coringa`.

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  /* app.enableCors({
-    origin: [process.env.CORS_URL],
-  }); */
+  app.enableCors({
+    origin: '*',
+  });
 
   await app.listen(3000);
 }
