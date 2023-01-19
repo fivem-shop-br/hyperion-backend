28ac4e861935a730e7d61f8546e5b1a075db4905
Fulano de Tal <F3ZINH0HG@gmail.com>
2023-01-18 23:56:14 -0300

    chore: test cors.

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
-  app.enableCors({
-    origin: '*',
-  });
-
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
