66c0564eccb88320d05e2001816096710e98a366
Fulano de Tal <F3ZINH0HG@gmail.com>
2023-01-28 03:26:57 -0300

    refactor(categories): rename for `bySlug`.

--- src/app/use-cases/categorie/find-categories.ts
+++ src/app/use-cases/categorie/find-categories.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { Categorie } from 'src/app/entities/categorie';
 import { CategorieRepository } from 'src/app/repositories/categorie-repository';
 
-export interface findAllCategorieResponse {
+export interface FindAllBySlugCategorieResponse {
   categorie: Categorie[];
 }
 
 @Injectable()
-export class FindAllCategorie {
+export class FindAllBySlugCategorie {
   constructor(private categorieRepository: CategorieRepository) {}
 
-  async execute(): Promise<findAllCategorieResponse> {
-    const categorie = await this.categorieRepository.findAll();
+  async execute(slug: string): Promise<FindAllBySlugCategorieResponse> {
+    const categorie = await this.categorieRepository.findAllBySlug(slug);
 
     return {
       categorie,
