6d5db5532e7443af03307d90fca402f82e9ccca5
Fulano de Tal <F3ZINH0HG@gmail.com>
2023-01-28 03:25:24 -0300

    feat(categorie): created more finds.

--- src/app/repositories/categorie-repository.ts
+++ src/app/repositories/categorie-repository.ts
@@ -2,8 +2,10 @@ import { Categorie } from '../entities/categorie';
 import type { Categories as CategoriePrisma } from '@prisma/client';
 
 export abstract class CategorieRepository {
-  abstract findAll(): Promise<Categorie[]>;
-  abstract findByName(name: string): Promise<Categorie[]>;
+  abstract findAllBySlug(slug: string): Promise<Categorie[]>;
+  abstract findByName(name: string, slug: string): Promise<Categorie>;
+  abstract findById(id: string): Promise<Categorie>;
+  abstract findBySlug(slug: string): Promise<Categorie>;
   abstract create(categorie: Categorie): Promise<CategoriePrisma>;
   abstract update(categorie: Categorie): Promise<CategoriePrisma>;
   abstract delete(categorieId: string): Promise<Categorie>;
