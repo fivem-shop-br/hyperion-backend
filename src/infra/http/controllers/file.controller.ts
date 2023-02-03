cf1646cbc716c2d147bffc01b171bec71e2e7a34
Fulano de Tal <F3ZINH0HG@gmail.com>
2023-02-03 05:14:06 -0300

    feat(file): update and connect aws s3.

---
+++ src/infra/http/controllers/file.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+  ParseFilePipe,
+  MaxFileSizeValidator,
+  FileTypeValidator,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FileUploadService } from 'src/utils/multer.service';
+
+@Controller()
+export class FileController {
+  constructor(private fileUploadService: FileUploadService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 5 * 1024 * 1024 }),
+          new FileTypeValidator({ fileType: /(jpg|jpeg|png|gif)$/ }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return await this.fileUploadService.uploadFile(
+      file.buffer,
+      file.originalname.replace(/\s/g, ''),
+      file.mimetype,
+    );
+  }
+}
